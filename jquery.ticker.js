1b94718c33432688839da233c3b7838072e4c191
Joe Wilson <joe@joecode.com>
2011-12-19 13:42:49 -0800

    Update to support interval attribute, fixed height to prevent jumpy content, and no fade on single items.

--- jquery.ticker.js
+++ jquery.ticker.js
@@ -1,27 +1,38 @@
 jQuery.fn.ticker = function(settings) {
+	
+	var ticker = $(this);
+	var itemsCollection =  ticker.find('li');
+	
+	var current=0;
+	var paused=0;
+	var interval=4000;
+	if (ticker.attr('interval')) interval = ticker.attr('interval');
+	
+	itemsCollection.each(function() {
+		if ($(this).height() > ticker.height()) {
+			ticker.height($(this).height());
+		}
+	});
 
-  var current=0;
-  var paused=0;
-  var newsTicker = $(this);
-  var itemsCollection =  newsTicker.find('li');
+	itemsCollection.hide().filter(":eq(0)").show().add("li");
 
-  itemsCollection.hide().filter(":eq(0)").show().add("li");
+	itemsCollection.hover(
+		function(){ paused=1; },
+		function(){ paused=0; }
+	);
 
-  itemsCollection.hover(
-    function(){ paused=1; },
-    function(){ paused=0; }
-  );
+	function tick() {
+		if (paused){ return; };
 
-  function ticknews() {
-    if (paused){ return; };
+		ticker.find("li:eq("+current+")").fadeOut("slow",function(){
+		  $(this).hide();
+		});
 
-    newsTicker.find("li:eq("+current+")").fadeOut("slow",function(){
-      $(this).hide();
-    });
-
-    current = ++current%itemsCollection.size();
-    newsTicker.find("li:eq("+current+")").fadeIn("slow");
-  }
-
-  setInterval(ticknews,4000);
+		current = ++current%itemsCollection.size();
+		ticker.find("li:eq("+current+")").fadeIn("slow");
+	}
+	
+	if (itemsCollection.length > 1) {
+		setInterval(tick,interval);
+	}
 };
